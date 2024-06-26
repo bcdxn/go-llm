f0e86ce7b1b635f67c7dbcfd07c0694de1d2ec8b
bcdxn <5067649+bcdxn@users.noreply.github.com>
2024-06-26 08:59:39 -0400

    fix: add empty main func to make go install happy

--- internal/cli/app.go
+++ internal/cli/app.go
@@ -2,6 +2,7 @@ package cli
 
 import (
 	"fmt"
+	"plugin"
 
 	"github.com/bcdxn/go-llm/internal/chat"
 	"github.com/urfave/cli/v2"
@@ -52,6 +53,32 @@ func New() *cli.App {
 				Usage:   "Send a message in-line to a model",
 				Action: func(*cli.Context) error {
 					fmt.Println("comming soon...")
+					return nil
+				},
+			},
+			{
+				Name:  "lp",
+				Usage: "load plugin test",
+				Action: func(*cli.Context) error {
+					p, err := plugin.Open("/Users/bdxn/Documents/2024-interview-prep/projects/go-llm-plugin-openai/go-llm-plugin-openai.so")
+					if err != nil {
+						fmt.Println("something bad happened loading the plugin...")
+						fmt.Println(err)
+						panic(err)
+					}
+
+					v, err := p.Lookup("V")
+					if err != nil {
+						panic(err)
+					}
+					f, err := p.Lookup("F")
+					if err != nil {
+						panic(err)
+					}
+
+					*v.(*int) = 7
+					f.(func())()
+
 					return nil
 				},
 			},
